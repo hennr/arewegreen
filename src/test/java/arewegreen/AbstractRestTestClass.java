e6a19b5a8ebad161254fc9f68d13fe58e6d34d53
Jan-Hendrik Peters <hennr@hennr.name>
2016-06-03 20:18:06 +0200

    use random port for a greater testing stability

--- src/test/java/arewegreen/AbstractRestTestClass.java
+++ src/test/java/arewegreen/AbstractRestTestClass.java
@@ -3,19 +3,23 @@ package arewegreen;
 import com.jayway.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
-@WebAppConfiguration
-@IntegrationTest("server.port:8090")
+@TestPropertySource(locations="classpath:test.properties")
+@WebIntegrationTest(randomPort = true)
 public class AbstractRestTestClass {
+
+    @Value("${local.server.port}")
+    private int port;
 
     @Before
     public void setUp() {
-        RestAssured.port = 8090;
+        RestAssured.port = port;
     }
 }
--- src/test/java/arewegreen/SpringContextTest.java
+++ src/test/java/arewegreen/SpringContextTest.java
@@ -2,13 +2,15 @@ package arewegreen;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@SpringApplicationConfiguration(Application.class)
+@TestPropertySource(locations="classpath:test.properties")
+@WebIntegrationTest(randomPort = true)
 public class SpringContextTest {
 
 	@Test
