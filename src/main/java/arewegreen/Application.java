79f6d35ac3ccba5d4fcabab185c87c45b69f8b54
Jan-Hendrik Peters <hennr@hennr.name>
2016-07-16 21:42:11 +0200

    find BDF via component scan only

--- src/main/java/arewegreen/Application.java
+++ src/main/java/arewegreen/Application.java
@@ -1,19 +1,12 @@
 package arewegreen;
 
-import arewegreen.browser.BrowserDriverFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(ignoreResourceNotFound = true, value = "file:${user.home}/arewegreen/application.properties")
 public class Application {
-
-    @Bean
-    BrowserDriverFactory browserDriverFactory() {
-        return new BrowserDriverFactory();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
