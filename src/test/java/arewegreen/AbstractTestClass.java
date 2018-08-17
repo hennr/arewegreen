0621adb5beef6c791f6048bc2d51280288f6ef4b
Vivian Zaddach <vivian.zaddach@idealo.de>
2018-08-17 08:54:53 +0200

    add test which checks if configuration file is generated

--- src/test/java/arewegreen/AbstractTestClass.java
+++ src/test/java/arewegreen/AbstractTestClass.java
@@ -1,8 +1,17 @@
 package arewegreen;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -20,5 +29,19 @@ public class AbstractTestClass {
     @Before
     public void setUp() {
         RestAssured.port = port;
+    }
+
+    @Test
+    public void checkIfConfigurationIsGenerated() throws IOException {
+        File configurationFile = new File("/tmp/arewegreen/application.properties");
+        assertTrue(configurationFile.exists());
+
+        FileInputStream applicationPropertiesStream = new FileInputStream(configurationFile);
+        Properties properties = new Properties();
+        properties.load(applicationPropertiesStream);
+
+        assertThat(properties.getProperty("startBrowserAutomatically").isEmpty(), is(false));
+        assertThat(properties.getProperty("scriptTimeoutInSeconds").isEmpty(), is(false));
+
     }
 }
