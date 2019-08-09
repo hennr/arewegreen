0607aeee95efaa4a1037fea7f8cfad1fafc4348d
Jan-Hendrik Peters <hennr@hennr.name>
2019-08-09 23:36:07 +0200

    raw styling of tiles

--- src/main/js/components/Tile.js
+++ src/main/js/components/Tile.js
@@ -3,10 +3,12 @@ import * as React from "react";
 export default class Tile extends React.Component {
     render() {
         return (
-            <div className="comets">
-                <div className="dashbot-col-visual__green">
-                    <div data-test-value="">{this.props.value}</div>
-                    <div data-test-text="">{this.props.text}</div>
+            <div className="tile">
+                <div className="comets">
+                    <div className="dashbot-col-visual__green">
+                        <div data-test-value="">{this.props.value}</div>
+                        <div data-test-text="">{this.props.text}</div>
+                    </div>
                 </div>
             </div>
         );
