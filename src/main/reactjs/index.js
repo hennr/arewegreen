904a0f005da40cf8879926a239a1080cefc18e96
Jan-Hendrik Peters <hennr@hennr.name>
2019-10-19 11:40:00 +0200

    drop angular and deliver reactJS app at / instead

--- src/main/reactjs/index.js
+++ src/main/reactjs/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./app/stylesheets/styl/app.styl";
+import "./styl/app.styl";
 import Tile from "./components/Tile";
 
 const App = () => (
