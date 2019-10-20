09da5da26de11aeddf629d09876b7118d8ba70e9
Jan-Hendrik Peters <hennr@hennr.name>
2019-10-20 09:26:43 +0200

    introduce internal state and constructor to have the component fetch his own data

--- src/main/reactjs/components/Tile.js
+++ src/main/reactjs/components/Tile.js
@@ -1,10 +1,18 @@
 import * as React from "react";
 
 export default class Tile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value
+        };
+    }
+
     render() {
         return (
                 <div className="tile green comets">
-                    <div className="value" data-test-value="">{this.props.value}</div>
+                    <div className="value" data-test-value="">{this.state.value}</div>
                     <div className="text" data-test-text="">{this.props.text}</div>
             </div>
         );
