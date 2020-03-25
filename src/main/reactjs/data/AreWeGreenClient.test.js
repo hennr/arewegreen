a4b59f1ebf5d3208bfd214b7f986cea750769111
Jan-Hendrik Peters <hennr@hennr.name>
2020-03-25 21:27:19 +0100

    migrate test from mockserver to axios-mock-adapter for speed an stability reasons

--- src/main/reactjs/data/AreWeGreenClient.test.js
+++ src/main/reactjs/data/AreWeGreenClient.test.js
@@ -1,58 +1,31 @@
-import mockServerNode from "mockserver-node";
-import mockServerClient from "mockserver-client";
 import {fetchData} from "./AreWeGreenClient";
 
-beforeAll(async () => {
-    await mockServerNode.start_mockserver({
-        serverPort: 8080,
-        trace: true,
-        jvmOptions: '-Dmockserver.enableCORSForAllResponses=true'
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+describe("AreWeGreenClient", () => {
+
+    let mock;
+
+    beforeEach(() => {
+        mock = new MockAdapter(axios);
     });
-});
 
-afterAll(async () => {
-    await mockServerNode.stop_mockserver({
-        serverPort: 8080
+    afterEach(() => {
+        mock.restore();
     });
-});
 
-test('fetches data on fetchData call', async () => {
-    // given
-    const expectedResult = '666';
-    const mockedPath = '/getMe';
-    const mockHttpServer = mockServerClient.mockServerClient;
-    await mockHttpServer('127.0.0.1', 8080)
-        .mockAnyResponse(
-            {
-                httpRequest: {
-                    method: 'GET',
-                    path: mockedPath,
-                },
-                httpResponse: {
-                    statusCode: 200,
-                    'body': JSON.stringify({value: expectedResult}),
-                    delay: {
-                        timeUnit: 'MILLISECONDS',
-                        value: 0
-                    },
-                },
-                times: {
-                    remainingTimes: 5,
-                    unlimited: false
-                }
-            }
-        )
-        .then(
-            (result) => {
-                console.log('mock server training finished');
-            },
-            (error) => {
-                console.log(`training the mock server went wrong: ` + error);
-            }
-        );
-
-    // when
-    const result = await fetchData(mockedPath);
-    // then
-    expect(result.data.value).toEqual(expectedResult);
+    test('fetches data on fetchData call', async () => {
+        // given
+        const expectedResult = '666';
+        const urlPath = "foo"
+        const mock = new MockAdapter(axios);
+        mock.onGet(`http://127.0.0.1:8080/${urlPath}`).reply(200, {
+            value: expectedResult,
+        });
+        // when
+        const result = await fetchData(urlPath);
+        // then
+        expect(result.data.value).toEqual(expectedResult);
+    });
 });
--- src/main/reactjs/layout/LayoutClient.test.js
+++ src/main/reactjs/layout/LayoutClient.test.js
@@ -1,69 +1,31 @@
-import mockServerNode from "mockserver-node";
-import mockServerClient from "mockserver-client";
-import {fetchLayout} from "./LayoutClient";
+import LayoutClient from "./LayoutClient";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
-beforeAll(async () => {
-    await mockServerNode.start_mockserver({
-        serverPort: 8080,
-        trace: true,
-        jvmOptions: '-Dmockserver.enableCORSForAllResponses=true'
+describe("LayoutClient", () => {
+
+    let mock;
+
+    beforeEach(() => {
+        mock = new MockAdapter(axios);
     });
-});
 
-afterAll(async () => {
-    await mockServerNode.stop_mockserver({
-        serverPort: 8080
+    afterEach(() => {
+        mock.restore();
     });
-});
 
-test('fetches layout.json on fetchLayout call', async () => {
-    // given
-    const expectedResult = `[
-\t[
-\t\t{ "visual": "number", "class": "storm", "title": "the lucky number",  "xhr": "/data?source=demo.sh", "xhrValue": "value", "xhrInterval": 10}
-\t],
-\t[
-\t\t{ "visual": "number", "class": "comets", "build": "green", "value": "OK", "title": "Build" },
-\t\t{ "visual": "number", "class": "desert", "unit": "percent", "build": "orange", "value": "82", "title": "Coverage" }
-\t],
-\t[
-\t\t{ "visual": "number", "class": "waterfall", "title": "https://github.com/hennr/arewegreen/wiki", "build": "none", "value": "docs"}
-\t]
-]
-`;
-    const mockHttpServer = mockServerClient.mockServerClient;
-    await mockHttpServer('127.0.0.1', 8080)
-        .mockAnyResponse(
-            {
-                httpRequest: {
-                    method: 'GET',
-                    path: '/layout.json',
-                },
-                httpResponse: {
-                    statusCode: 200,
-                    'body': {value: expectedResult},
-                    delay: {
-                        timeUnit: 'MILLISECONDS',
-                        value: 0
-                    },
-                },
-                times: {
-                    remainingTimes: 5,
-                    unlimited: false
-                }
-            }
-        )
-        .then(
-            (result) => {
-                console.log('mock server training finished');
-            },
-            (error) => {
-                console.log(`training the mock server went wrong: ` + error);
-            }
-        );
+    test('fetches layout.json on fetchLayout call', async () => {
+        // given
+        const expectedResult = "dummy return value";
 
-    // when
-    const result = await fetchLayout();
-    // then
-    expect(result.data.value).toEqual(expectedResult);
+        const mock = new MockAdapter(axios);
+        mock.onGet(`http://127.0.0.1:8080/layout.json`).reply(200, {
+            value: expectedResult,
+        });
+
+        // when
+        const result = await new LayoutClient().fetchLayout();
+        // then
+        expect(result.data.value).toEqual(expectedResult);
+    });
 });
