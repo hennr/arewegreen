6844715211e4116396fa16b742b6c00b1106cdea
Jan-Hendrik Peters <hennr@hennr.name>
2020-03-29 21:31:34 +0200

    flowify Error.js

--- src/main/reactjs/components/Error.js
+++ src/main/reactjs/components/Error.js
@@ -1,3 +1,11 @@
-export default function Error(props) {
+// @flow
+
+import * as React from 'react';
+
+type Props = {
+    errorMessage: string
+};
+
+export default function Error(props: Props) {
     return <div>{props.errorMessage}</div>;
 }
