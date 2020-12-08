1b768f830940730b29cf459dacf91596891ce0ee
Jan-Hendrik Peters <hennr@hennr.name>
2020-12-08 22:58:44 +0100

    migrate to typescript

--- src/main/reactjs/components/App.test.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import App from "./App"
-
-describe("App", () => {
-    it("renders without errors", () => {
-        render(<App/>);
-    });
-});
--- src/main/reactjs/data/AreWeGreenDataClient.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import axios from "axios";
-import type {$AxiosXHR} from "axios";
-
-export function fetchData(path: string): Promise<$AxiosXHR<*,*>> {
-    return axios.get('/' + path)
-}
